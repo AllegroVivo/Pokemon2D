a653135f1b1d979844e942a5130dd2d283db2c80
Stephanie Phillips <StephBotCoder@gmail.com>
2024-02-22 21:25:09 -0800

    Video #43 Completed

---
+++ Assets/Scripts/Core/Fader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fader : MonoBehaviour
+{
+    private Image _image;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    public IEnumerator FadeIn(Single fadeTime)
+    {
+        yield return _image.DOFade(1f, fadeTime).WaitForCompletion();
+    }
+
+    public IEnumerator FadeOut(Single fadeTime)
+    {
+        yield return _image.DOFade(0f, fadeTime).WaitForCompletion();
+    }
+}
--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -11,10 +11,16 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
     [SerializeField] private DestinationID _destination;
 
     private PlayerController _player;
+    private Fader _fader;
 
     public Transform SpawnPoint => _spawnPoint;
     public DestinationID Destination => _destination;
-    
+
+    private void Start()
+    {
+        _fader = FindObjectOfType<Fader>();
+    }
+
     public void OnPlayerTriggered(PlayerController player)
     {
         _player = player;
@@ -26,12 +32,14 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
         DontDestroyOnLoad(gameObject);
 
         GameController.I.PauseGame(true);
+        yield return _fader.FadeIn(0.5f);
         
         yield return SceneManager.LoadSceneAsync(_sceneToLoad);
 
         Portal destPortal = FindObjectsOfType<Portal>().First(x => x != this && x.Destination == _destination);
         _player.Character.SetPositionAndSnapToTile(destPortal.SpawnPoint.position);
 
+        yield return _fader.FadeOut(0.5f);
         GameController.I.PauseGame(false);
 
         Destroy(gameObject);
